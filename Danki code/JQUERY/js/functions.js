4d20f2bbc4479a22f34ec8034af140d2b5a6b535
Dpalma1979 <danielnpalma@hotmail.com>
2020-05-29 14:24:44 -0300

    estudos front end

---
+++ Danki code/JQUERY/js/functions.js
@@ -0,0 +1,22 @@
+
+/*
+$(function(){
+
+	alert("ola mundo");
+});
+*/
+
+/*
+
+$(document).ready(function(){
+
+	alert("Boa Noite");
+});
+*/
+
+$(window).on('load', function(){
+
+	alert("Deus é Fiel");
+});
+
+// Está são as três maneiras de chamar funções  do JQuery.
---
+++ Danki code/JavaScript/AulasPraticas/function.js
@@ -0,0 +1,10 @@
+
+
+		window.onload = function(){
+		
+        var idade = 2013 - 2020;
+
+        if (idade > 1) {
+        	console.log('condição verdadeira');
+        }
+	}
